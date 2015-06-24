d6327b9be4ddb9d20222c4e1eac4158d4fc2815d
Vadim Axelrod <vakselrod@cinimex.ru>
2015-06-25 00:57:19 +0300

    Initial commit

---
+++ src/wmb/utils/message/xmlnsc/NamespaceUtils.java
@@ -0,0 +1,15 @@
+package wmb.utils.message.xmlnsc;
+
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbMessageAssembly;
+
+public class NamespaceUtils extends MbJavaComputeNode {
+
+	@Override
+	public void evaluate(MbMessageAssembly arg0) throws MbException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
